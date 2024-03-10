423d33db215f417d5debe23eb2181372cc8116b3
Dmytro Popovskyi <popovskiy96@gmail.com>
2024-03-10 20:38:10 +0100

    remove default vite markup

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './components/App/App.jsx'
+import '../node_modules/modern-normalize/modern-normalize.css'
+import '../node_modules/reset-css/reset.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
